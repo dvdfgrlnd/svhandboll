2e4f99bb04aad3c63c93669895b32a168dc1f2ff
david <dvdfgrlnd@gmail.com>
2021-10-12 19:38:28 +0200

    Add api calls

--- index.js
+++ index.js
@@ -5,3 +5,36 @@ fetch("http://localhost:8000/test")
     }).catch(error => {
         console.error(error);
     });
+
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+function createDropdown(name, values) {
+    // Create select
+    let select = htmlToElement(`<select id="dropdown_${name}"></select>`);
+    // Create search field
+    let searchfield = htmlToElement(`<input type="text" id="searchfield_${name}" name="" size="10">`);
+    // Add values
+    values.forEach((elem) => {
+        let opt = htmlToElement(`<option value="${elem.id}">${elem.value}</option>`);
+        select.appendChild(opt);
+    });
+
+    let container = htmlToElement(`<div class="container"></div>`);
+    container.appendChild(select);
+    container.appendChild(searchfield);
+
+    return container;
+}
+
+let blank = {id:"___blank___", value:""};
+let c1 = createDropdown("test", [blank, {id:"id1", value:"value1"},{id:"id2", value:"value2"}]);
+let c2 = createDropdown("test2", [blank, {id:"id2", value:"value2"}]);
+
+let con = document.querySelector("#container");
+con.appendChild(c1);
+con.appendChild(c2);
--- src/main.rs
+++ src/main.rs
@@ -3,112 +3,90 @@ extern crate rocket;
 
 use isahc::{prelude::*, Request as IsahcRequest};
 
-
-use rocket::{Request, Data, Response};
 use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::{Method, ContentType, Status, Header};
+use rocket::http::{ContentType, Header, Method, Status};
+use rocket::{Data, Request, Response};
 
 struct Counter;
 
 #[rocket::async_trait]
 impl Fairing for Counter {
-
     // This is a request and response fairing named "GET/POST Counter".
     fn info(&self) -> Info {
         Info {
             name: "GET/POST Counter",
-            kind: Kind::Request | Kind::Response
+            kind: Kind::Request | Kind::Response,
         }
     }
 
-    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-        // // Don't change a successful user's response, ever.
-        // if response.status() != Status::NotFound {
-        //     return
-        // }
-
-        // // Rewrite the response to return the current counts.
-        // if request.method() == Method::Get && request.uri().path() == "/counts" {
-        //     let get_count = self.get.load(Ordering::Relaxed);
-        //     let post_count = self.post.load(Ordering::Relaxed);
-        //     let body = format!("Get: {}\nPost: {}", get_count, post_count);
-
-        //     response.set_status(Status::Ok);
-        //     response.set_header(ContentType::Plain);
-        //     response.set_sized_body(body.len(), Cursor::new(body));
-        // }
     }
 }
-// pub struct CORS;
 
-// impl Fairing for CORS {
-//     fn info(&self) -> Info {
-//         Info {
-//             name: "Add CORS headers to responses",
-//             kind: Kind::Response
-//         }
-//     }
-
-//     fn on_response(&self, request: &Request, response: &mut Response) {
-//         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-//         response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
-//         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-//         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-//     }
-// }
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/test")]
-fn index2() -> Result<String, String> {
-    // Send a GET request and wait for the response headers.
-    // Must be `mut` so we can read the response body.
+type HttpError = String;
 
-    // let response = isahc::get("https://www.google.com");
-    let url = "https://services.svenskhandboll.se/v1/api/County/ListCounties?APIKey=28c7X8AK";
-    //  -H 'Referer: '
-    let re = IsahcRequest::builder()
+fn make_request(url: &str) -> Result<String, HttpError> {
+    let result = IsahcRequest::builder()
         .uri(url)
         .header("Referer", "https://ta.svenskhandboll.se/")
-        .body(());
-    let response2 = match re {
-        Ok(r) => {
-            let response2 = match r.send() {
-                Ok(mut r) => {
-                    // Print some basic info about the response to standard output.
-                    println!("Status: {}", r.status());
-                    println!("Headers: {:#?}", r.headers());
+        .body(())
+        .map_err(|x| x.to_string())
+        .and_then(|x| x.send().map_err(|x| x.to_string()))
+        .map(|mut r| {
+            // Print some basic info about the response to standard output.
+            // println!("Status: {}", r.status());
+            // println!("Headers: {:#?}", r.headers());
 
-                    // Read the response body as text into a string and print it.
-                    let t = r.text().unwrap_or("error".to_string());
-                    print!("{}", t);
+            // Read the response body as text into a string and print it.
+            let text = r.text().unwrap_or_else(|x| x.to_string());
 
-                    t
-                }
-                Err(e) => {
-                    // "error2".to_string()
-                    format!("{:?}", e)
-                }
-            };
+            text
+        });
 
-            response2
-        }
-        Err(_) => "".to_string()
-    };
-
-    Ok(response2)
+    result
 }
+
+#[get("/listcounties")]
+fn listcounties() -> Result<String, String> {
+    let url = "https://services.svenskhandboll.se/v1/api/County/ListCounties?APIKey=28c7X8AK";
+    make_request(url)
+}
+
+#[get("/listteams?<season>")]
+fn listteams(season: &str) -> Result<String, String> {
+    let url = format!("https://services.svenskhandboll.se/v1/api/Team/ListTeamsByClubAndSeasonWithFavorites?APIKey=28c7X8AK&clubId=&seasonId={}&favIds=0", season);
+    make_request(&url)
+}
+
+#[get("/listdivisions?<season>&<countyid>")]
+fn listdivisions(season: &str, countyid: Option<&str>) -> Result<String, String> {
+    let url = format!("https://services.svenskhandboll.se/v1/api/Division/ListDivisionsWithFavorites?APIKey=28c7X8AK&favIds=0&countyId={}&seasonId={}", countyid.unwrap_or(""), season);
+    make_request(&url)
+}
+
+#[get("/listseasons")]
+fn listseasons() -> Result<String, String> {
+    let url = "https://services.svenskhandboll.se/v1/api/Season/ListSeasons?APIKey=28c7X8AK";
+    make_request(url)
+}
+
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![index, index2])
-    .attach(Counter)
+        .mount("/", routes![index, listcounties, listteams, listdivisions, listseasons])
+        .attach(Counter)
 }
