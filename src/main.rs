300f5b7322999fb2358eadde14b7aac54bc445c1
david <dvdfgrlnd@gmail.com>
2021-10-12 14:26:57 +0200

    :Create index.html, update rustconfig

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,65 @@
 #[macro_use]
 extern crate rocket;
 
-use isahc::{prelude::*, Request};
+use isahc::{prelude::*, Request as IsahcRequest};
+
+
+use rocket::{Request, Data, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{Method, ContentType, Status, Header};
+
+struct Counter;
+
+#[rocket::async_trait]
+impl Fairing for Counter {
+
+    // This is a request and response fairing named "GET/POST Counter".
+    fn info(&self) -> Info {
+        Info {
+            name: "GET/POST Counter",
+            kind: Kind::Request | Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+        // // Don't change a successful user's response, ever.
+        // if response.status() != Status::NotFound {
+        //     return
+        // }
+
+        // // Rewrite the response to return the current counts.
+        // if request.method() == Method::Get && request.uri().path() == "/counts" {
+        //     let get_count = self.get.load(Ordering::Relaxed);
+        //     let post_count = self.post.load(Ordering::Relaxed);
+        //     let body = format!("Get: {}\nPost: {}", get_count, post_count);
+
+        //     response.set_status(Status::Ok);
+        //     response.set_header(ContentType::Plain);
+        //     response.set_sized_body(body.len(), Cursor::new(body));
+        // }
+    }
+}
+// pub struct CORS;
+
+// impl Fairing for CORS {
+//     fn info(&self) -> Info {
+//         Info {
+//             name: "Add CORS headers to responses",
+//             kind: Kind::Response
+//         }
+//     }
+
+//     fn on_response(&self, request: &Request, response: &mut Response) {
+//         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+//         response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+//         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+//         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+//     }
+// }
 
 #[get("/")]
 fn index() -> &'static str {
@@ -16,7 +74,7 @@ fn index2() -> Result<String, String> {
     // let response = isahc::get("https://www.google.com");
     let url = "https://services.svenskhandboll.se/v1/api/County/ListCounties?APIKey=28c7X8AK";
     //  -H 'Referer: '
-    let re = Request::builder()
+    let re = IsahcRequest::builder()
         .uri(url)
         .header("Referer", "https://ta.svenskhandboll.se/")
         .body(());
@@ -50,5 +108,7 @@ fn index2() -> Result<String, String> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, index2])
+    rocket::build()
+    .mount("/", routes![index, index2])
+    .attach(Counter)
 }
